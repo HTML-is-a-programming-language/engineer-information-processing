2475cd00557ce8af1930b6e15200e76a34aed806
seungjunseo <newworld1423@naver.com>
2025-08-08 17:12:29 +0900

    feat: 임시 커밋

---
+++ src/app/past/page.tsx
@@ -0,0 +1,10 @@
+// src/app/past/page.tsx
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "과년도 풀이",
+};
+
+export default function PastPage() {
+  return <main className="px-4 py-10">과년도 페이지</main>;
+}
